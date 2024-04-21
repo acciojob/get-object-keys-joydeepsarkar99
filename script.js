90ce4d921ed46b4b7b807843e610b31c92ba7755
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-04-21 18:22:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const student = {
+	name
+}
+student.prototype.getKeys = function(){
+	let result = [];
+	for(let key in student){
+		result.push(key);
+	}
+	return result;
+}
